ceb505f4dcaf27d60cbbd528f29507a7424479fd
Andy <andy_sun_sha@hotmail.com>
2017-10-02 19:50:47 +0200

    A few more simple tests.

--- tests/tests.c
+++ tests/tests.c
@@ -1,18 +1,27 @@
 #include <check.h>
-#include <stdio.h>
 #include "memory.h"
 #include "cpu.h"
 #include "registers.h"
 
-void setUp(unsigned char instruction)
+void setUpInstruction(unsigned char instruction, void *value)
 {
     reset();
     debugWriteByte(0x100, instruction);
+
+    if (instructions[instruction].operand_length == 1)
+    {
+        debugWriteByte(0x101, *((unsigned char *) value));
+    }
+    else if (instructions[instruction].operand_length == 2)
+    {
+        debugWriteShort(0x101, *((unsigned short *) value));
+    }
 }
 
 START_TEST(test_nop)
     {
-        setUp(0x0);
+        setUpInstruction(0x0, 0);
+
         cycle();
         ck_assert(flagIsSet(FLAG_Z) == 0);
         ck_assert(flagIsSet(FLAG_N) == 0);
@@ -24,18 +33,50 @@ END_TEST
 
 START_TEST(test_ld_bc_nn)
     {
-        setUp(0x1);
-        unsigned short address = 0x101;
-        unsigned short val = 0xFF;
-        debugWriteShort(address, val);
+        unsigned short val = 0xFFFF;
+        setUpInstruction(0x1, &val);
+
         cycle();
         ck_assert(flagIsSet(FLAG_Z) == 0);
         ck_assert(flagIsSet(FLAG_N) == 0);
         ck_assert(flagIsSet(FLAG_H) == 0);
         ck_assert(flagIsSet(FLAG_C) == 0);
-        ck_assert(readShort(address) == 0xFF);
-        ck_assert(registers.bc == 0xFF);
+        ck_assert(registers.bc == val);
         ck_assert(registers.pc == 0x103);
+    }
+END_TEST
+
+START_TEST(test_ld_bc_v_a)
+    {
+        unsigned char val = 0xFF;
+        unsigned short address = 0x8000;
+        setUpInstruction(0x2, 0);
+
+        registers.a = val;
+        registers.bc = address;
+        cycle();
+        ck_assert(flagIsSet(FLAG_Z) == 0);
+        ck_assert(flagIsSet(FLAG_N) == 0);
+        ck_assert(flagIsSet(FLAG_H) == 0);
+        ck_assert(flagIsSet(FLAG_C) == 0);
+        ck_assert(readByte(address) == val);
+        ck_assert(registers.pc == 0x101);
+    }
+END_TEST
+
+START_TEST(test_inc_bc)
+    {
+        unsigned short val = 0x1;
+        setUpInstruction(0x3, 0);
+
+        registers.bc = val;
+        cycle();
+        ck_assert(flagIsSet(FLAG_Z) == 0);
+        ck_assert(flagIsSet(FLAG_N) == 0);
+        ck_assert(flagIsSet(FLAG_H) == 0);
+        ck_assert(flagIsSet(FLAG_C) == 0);
+        ck_assert(registers.bc == val + 1);
+        ck_assert(registers.pc == 0x101);
     }
 END_TEST
 
@@ -50,6 +91,8 @@ Suite * cpu_suite(void)
 
     tcase_add_test(tc_core, test_nop);
     tcase_add_test(tc_core, test_ld_bc_nn);
+    tcase_add_test(tc_core, test_ld_bc_v_a);
+    tcase_add_test(tc_core, test_inc_bc);
     suite_add_tcase(s, tc_core);
     return s;
 }
